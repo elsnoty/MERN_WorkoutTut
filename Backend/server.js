e4f9116f547dc7d69a60366dcbec8cc4ac1871c0
elsnoty <mostafashawky399@gmail.com>
2024-09-05 20:33:45 +0300

    HOTfix#1

--- Backend/server.js
+++ Backend/server.js
@@ -11,21 +11,26 @@ const app = express();
 app.use(express.json()); // for req post and update
 
 // CORS configuration to allow requests from specific origins
+const allowedOrigins = ['http://localhost:5173', 'https://mern-workout-tut.vercel.app'];
+
 app.use(
-    cors({
-      origin: function (origin, callback) {
-        const allowedOrigins = ['http://localhost:5173', 'https://mern-workout-tut.vercel.app'];
-        if (!origin || allowedOrigins.includes(origin)) {
-          callback(null, true);
-        } else {
-          callback(new Error('Not allowed by CORS'));
-        }
-      },
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-      credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-    })
-  );
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin, like mobile apps or curl requests
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+  })
+);
+
+// Handle preflight requests
+app.options('*', cors());
 
 // routes
 app.use('/api/workouts/', WorkoutsRoutes);
